8d8a9229529f00c3531c74a62c4bda60851cebe5
Venkat <venkatp0326@gmail.com>
2021-09-10 16:54:45 -0400

    adding basics class

---
+++ src/test/java/com/github/venkatp0326/base/Basics.java
@@ -0,0 +1,21 @@
+package com.github.venkatp0326.base;
+
+public class Basics {
+   public static void main(String[] args)
+   {
+       System.out.println("Pravallika");
+       String name="Pravallika";
+      if(name.contains("s"))
+      {
+          System.out.println("name contains s");
+
+      }
+      name.substring(10);
+      int a=1460;
+      int b=100;
+      int sum=a+b;
+      int s=a%b;
+      System.out.println("Sum = "+sum);
+      System.out.println(s);
+   }
+}
